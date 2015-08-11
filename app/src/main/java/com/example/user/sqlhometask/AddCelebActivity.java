634f1accb732e33b1c6536a5b27f1b69e099b019
valeriandema <valerian.demichev@gmail.com>
2015-08-11 15:58:53 +0300

    Added AddCelebActivity

--- app/src/main/java/com/example/user/sqlhometask/AddCelebActivity.java
+++ app/src/main/java/com/example/user/sqlhometask/AddCelebActivity.java
@@ -1,5 +1,44 @@
 package com.example.user.sqlhometask;
 
-public class AddCelebActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+public class AddCelebActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration_celeb);
+
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        TextView txtGotDate = (TextView) findViewById(R.id.txtGotDate);
+        final EditText etDescription = (EditText) findViewById(R.id.etDescription);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        txtGotDate.setText(message);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentBack = new Intent();
+                intentBack.putExtra(MainActivity.EXTRA_MESSAGE, etDescription.getText().toString());
+                setResult(RESULT_OK, intentBack);
+                finish();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
